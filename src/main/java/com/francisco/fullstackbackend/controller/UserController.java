92bddf5843484d90593cae0eb3afcc363f0ab2a6
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-01 23:30:10 -0300

    "primer commit en back"

---
+++ src/main/java/com/francisco/fullstackbackend/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.francisco.fullstackbackend.controller;
+
+import com.francisco.fullstackbackend.exception.UserNotFoundException;
+import com.francisco.fullstackbackend.model.User;
+import com.francisco.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@CrossOrigin("http://localhost:3000/")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+
+
+}
