8eff3b6750b16c649217c5b180eb5a193997e208
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-01 23:40:45 -0300

    segundo commit

--- src/main/java/com/francisco/fullstackbackend/FullStackBackendApplication.java
+++ src/main/java/com/francisco/fullstackbackend/FullStackBackendApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FullStackBackendApplication {
 
+
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FullStackBackendApplication.class, args);
 	}
